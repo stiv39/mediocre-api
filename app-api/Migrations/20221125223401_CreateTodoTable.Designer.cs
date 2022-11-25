d729bb165d688614099f109f0729cb877bdff3d1
stiv39 <jarsinsky.norbert@gmail.com>
2022-11-25 23:46:04 +0100

    migrations && seed

--- app-api/Migrations/20221125223401_CreateTodoTable.Designer.cs
+++ app-api/Migrations/20221125223401_CreateTodoTable.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using app_data.Context;
 
@@ -10,9 +11,10 @@ using app_data.Context;
 namespace appapi.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20221125223401_CreateTodoTable")]
+    partial class CreateTodoTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
