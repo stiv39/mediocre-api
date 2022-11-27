8572f943da291e7ee4cb155183f3aefdbbba4974
stiv39 <jarsinsky.norbert@gmail.com>
2022-11-27 22:58:12 +0100

    add Cors

--- app-api/Program.cs
+++ app-api/Program.cs
@@ -29,6 +29,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors(c => c.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
